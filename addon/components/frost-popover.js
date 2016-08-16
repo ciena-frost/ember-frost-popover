da7d3968b11c4ac84fa7bc83e128af88f49476c6
Eric White <ericwhite613@gmail.com>
2016-08-16 15:34:33 -0400

    Top , bottom, left , right positions

--- addon/components/frost-popover.js
+++ addon/components/frost-popover.js
@@ -5,22 +5,23 @@ const ESC = 27
 
 export default Ember.Component.extend({
   layout,
-  isVisible: false,
+  visible: false,
   event: 'click',
   closest: false,
   position: 'bottom',
-  classNameBindings: ['position'],
+  index: 0,
+  classNameBindings: ['position', 'visible:visible:invisible'],
   classNames: ['tooltip-frost-popover'],
   didRender () {
     Ember.run.next(() => {
       const context = this
       if (this.get('closest')) {
         this.$().closest(this.get('target')).on(this.get('event'), function () {
-          context.toggleProperty('isVisible')
+          context.send('togglePopover')
         })
       } else {
         this.get('parentView').$(this.get('target')).on(this.get('event'), function () {
-          context.toggleProperty('isVisible')
+          context.send('togglePopover')
         })
       }
     })
@@ -28,7 +29,45 @@ export default Ember.Component.extend({
   actions: {
     close () {
       if (this.get('isDestroyed')) { return }
-      this.set('isVisible', false)
+      this.set('visible', false)
+    },
+    togglePopover () {
+      this.toggleProperty('visible')      
+      let position = this.get('position')
+      let targetRect
+      targetRect = this.get('closest') ? this.$().closest(this.get('target'))[this.get('index')].getBoundingClientRect() : this.get('parentView').$(this.get('target'))[this.get('index')].getBoundingClientRect()
+      let popoverElement = this.get('element')
+      let popoverRect = popoverElement.getBoundingClientRect()
+      let top
+      let bottom
+      let left
+      let right
+      switch (position) {
+        case 'bottom':
+          top = targetRect.bottom + 5
+          left = targetRect.left
+          popoverElement.style.top = top + 'px'
+          popoverElement.style.left = left + 'px'
+          break
+        case 'top':
+          top = targetRect.top - targetRect.height - 5
+          left = targetRect.left
+          popoverElement.style.top = top + 'px'
+          popoverElement.style.left = left + 'px'
+          break
+        case 'left':
+          top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
+          left = targetRect.left - targetRect.width + 34.4
+          popoverElement.style.top = top + 'px'
+          popoverElement.style.left = left + 'px'
+          break
+        case 'right':
+          top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
+          left = targetRect.right + 5
+          popoverElement.style.top = top + 'px'
+          popoverElement.style.left = left + 'px'
+          break
+      }
     }
   }
 })
