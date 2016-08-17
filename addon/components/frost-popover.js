b0928cca4f9dab52c3a8945b56ad446c5adeac50
Eric White <ericwhite613@gmail.com>
2016-08-17 17:27:27 -0400

    Fix absolute pos calculations + add exclude padding property

--- addon/components/frost-popover.js
+++ addon/components/frost-popover.js
@@ -1,15 +1,15 @@
-import $ from 'jquery'
 import Ember from 'ember'
 import layout from '../templates/components/frost-popover'
-const ESC = 27
 
 export default Ember.Component.extend({
   layout,
   visible: false,
   event: 'click',
   closest: false,
+  offset: 10,
   position: 'bottom',
   index: 0,
+  excludePadding: false,
   classNameBindings: ['position', 'visible:visible:invisible'],
   classNames: ['tooltip-frost-popover'],
   didRender () {
@@ -35,33 +35,51 @@ export default Ember.Component.extend({
       this.toggleProperty('visible')
       let position = this.get('position')
       let targetRect
-      targetRect = this.get('closest') ? this.$().closest(this.get('target'))[this.get('index')].getBoundingClientRect() : this.get('parentView').$(this.get('target'))[this.get('index')].getBoundingClientRect()
+      let targetElement = this.get('closest') ? this.$().closest(this.get('target'))[this.get('index')] : this.get('parentView').$(this.get('target'))[this.get('index')]
+      targetRect = targetElement.getBoundingClientRect()
       let popoverElement = this.get('element')
       let popoverRect = popoverElement.getBoundingClientRect()
       let top
       let left
+      let excludePadding = this.get('excludePadding')
       switch (position) {
         case 'bottom':
-          top = targetRect.bottom + 5
+          top = targetRect.bottom + this.get('offset')
           left = targetRect.left + targetRect.width / 2 - popoverRect.width / 2
+          if (excludePadding) {
+            let cs = window.getComputedStyle(targetElement)
+            top -= parseInt(cs.getPropertyValue('padding-bottom'))
+          }
           popoverElement.style.top = top + 'px'
           popoverElement.style.left = left + 'px'
           break
         case 'top':
-          top = targetRect.top - targetRect.height - 5
+          top = targetRect.top - popoverRect.height - this.get('offset')
           left = targetRect.left + targetRect.width / 2 - popoverRect.width / 2
+          if (excludePadding) {
+            let cs = window.getComputedStyle(targetElement)
+            top += parseInt(cs.getPropertyValue('padding-top'))
+          }
           popoverElement.style.top = top + 'px'
           popoverElement.style.left = left + 'px'
           break
         case 'left':
           top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
-          left = targetRect.left - targetRect.width + 34.4
+          left = targetRect.left - popoverRect.width - this.get('offset')
+          if (excludePadding) {
+            let cs = window.getComputedStyle(targetElement)
+            left += parseInt(cs.getPropertyValue('padding-left'))
+          }
           popoverElement.style.top = top + 'px'
           popoverElement.style.left = left + 'px'
           break
         case 'right':
           top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
-          left = targetRect.right + 5
+          left = targetRect.right + this.get('offset')
+          if (excludePadding) {
+            let cs = window.getComputedStyle(targetElement)
+            left -= parseInt(cs.getPropertyValue('padding-right'))
+          }
           popoverElement.style.top = top + 'px'
           popoverElement.style.left = left + 'px'
           break
