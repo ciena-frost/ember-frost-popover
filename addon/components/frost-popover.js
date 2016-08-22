7b1ff2b16fb3aa3cea116d03dfba3e9744f5b7d8
Eric White <ericwhite613@gmail.com>
2016-08-22 16:50:35 -0400

    add auto position

--- addon/components/frost-popover.js
+++ addon/components/frost-popover.js
@@ -1,6 +1,31 @@
 import Ember from 'ember'
 import layout from '../templates/components/frost-popover'
 import $ from 'jquery'
+
+function checkLeft (elementPosition, popoverRect, offset, result) {
+  if (elementPosition.left > 0 + popoverRect.width + offset) {
+    result = 'left'
+  }
+  return result
+}
+function checkRight (elementPosition, popoverRect, offset, result) {
+  if (elementPosition.left < 0 + popoverRect.width + offset) {
+    result = 'right'
+  }
+  return result
+}
+function checkTop (elementPosition, popoverRect, offset, result) {
+  if (elementPosition.top > 0 + popoverRect.height + offset) {
+    result = 'top'
+  }
+  return result
+}
+function checkBottom (elementPosition, popoverRect, offset, result) {
+  if (elementPosition.top < 0 + popoverRect.height + offset) {
+    result = 'bottom'
+  }
+  return result
+}
 export default Ember.Component.extend({
   layout,
   visible: false,
@@ -8,9 +33,10 @@ export default Ember.Component.extend({
   closest: false,
   offset: 10,
   position: 'bottom',
+  autoPosition: null,
   index: 0,
   excludePadding: false,
-  classNameBindings: ['position', 'visible:visible:invisible'],
+  classNameBindings: ['visible:visible:invisible', 'autoPosition'],
   classNames: ['tooltip-frost-popover'],
   didRender () {
     Ember.run.next(() => {
@@ -50,47 +76,77 @@ export default Ember.Component.extend({
       let top
       let left
       let excludePadding = this.get('excludePadding')
-      switch (position) {
-        case 'bottom':
-          top = targetRect.bottom + this.get('offset')
-          left = targetRect.left + targetRect.width / 2 - popoverRect.width / 2
-          if (excludePadding) {
-            let cs = window.getComputedStyle(targetElement)
-            top -= parseInt(cs.getPropertyValue('padding-bottom'))
-          }
-          popoverElement.style.top = top + 'px'
-          popoverElement.style.left = left + 'px'
-          break
-        case 'top':
-          top = targetRect.top - popoverRect.height - this.get('offset')
-          left = targetRect.left + targetRect.width / 2 - popoverRect.width / 2
-          if (excludePadding) {
-            let cs = window.getComputedStyle(targetElement)
-            top += parseInt(cs.getPropertyValue('padding-top'))
-          }
-          popoverElement.style.top = top + 'px'
-          popoverElement.style.left = left + 'px'
-          break
-        case 'left':
-          top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
-          left = targetRect.left - popoverRect.width - this.get('offset')
-          if (excludePadding) {
-            let cs = window.getComputedStyle(targetElement)
-            left += parseInt(cs.getPropertyValue('padding-left'))
-          }
-          popoverElement.style.top = top + 'px'
-          popoverElement.style.left = left + 'px'
-          break
-        case 'right':
-          top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
-          left = targetRect.right + this.get('offset')
-          if (excludePadding) {
-            let cs = window.getComputedStyle(targetElement)
-            left -= parseInt(cs.getPropertyValue('padding-right'))
-          }
-          popoverElement.style.top = top + 'px'
-          popoverElement.style.left = left + 'px'
-          break
+      let offset = this.get('offset')
+      var positions = position.split(' ')
+      console.log(positions)
+      if (positions[0] === 'auto') {
+        var elementPosition = targetElement.getBoundingClientRect()
+        let tempAutoPosition = null
+        if (positions[1] === 'left') {
+          tempAutoPosition = checkBottom(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkTop(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkRight(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkLeft(elementPosition, popoverRect, offset, tempAutoPosition)
+        } else if (positions[1] === 'right') {
+          tempAutoPosition = checkBottom(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkTop(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkLeft(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkRight(elementPosition, popoverRect, offset, tempAutoPosition)
+        } else if (positions[1] === 'bottom') {
+          tempAutoPosition = checkLeft(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkRight(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkTop(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkBottom(elementPosition, popoverRect, offset, tempAutoPosition)
+        } else {
+          tempAutoPosition = checkLeft(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkRight(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkBottom(elementPosition, popoverRect, offset, tempAutoPosition)
+          tempAutoPosition = checkTop(elementPosition, popoverRect, offset, tempAutoPosition)
+        }
+        console.log(tempAutoPosition)
+        this.set('autoPosition', tempAutoPosition)
+      }
+      let autoPosition = this.get('autoPosition')
+      if (positions[0] === 'bottom' || autoPosition === 'bottom') {
+        this.set('autoPosition', 'bottom')
+        top = targetRect.bottom + this.get('offset')
+        left = targetRect.left + targetRect.width / 2 - popoverRect.width / 2
+        if (excludePadding) {
+          let cs = window.getComputedStyle(targetElement)
+          top -= parseInt(cs.getPropertyValue('padding-bottom'))
+        }
+        popoverElement.style.top = top + 'px'
+        popoverElement.style.left = left + 'px'
+      } else if (positions[0] === 'top' || autoPosition === 'top') {
+        this.set('autoPosition', 'top')
+        top = targetRect.top - popoverRect.height - this.get('offset')
+        left = targetRect.left + targetRect.width / 2 - popoverRect.width / 2
+        if (excludePadding) {
+          let cs = window.getComputedStyle(targetElement)
+          top += parseInt(cs.getPropertyValue('padding-top'))
+        }
+        popoverElement.style.top = top + 'px'
+        popoverElement.style.left = left + 'px'
+      } else if (positions[0] === 'left' || autoPosition === 'left') {
+        this.set('autoPosition', 'left')
+        top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
+        left = targetRect.left - popoverRect.width - this.get('offset')
+        if (excludePadding) {
+          let cs = window.getComputedStyle(targetElement)
+          left += parseInt(cs.getPropertyValue('padding-left'))
+        }
+        popoverElement.style.top = top + 'px'
+        popoverElement.style.left = left + 'px'
+      } else if (positions[0] === 'right' || autoPosition === 'right') {
+        this.set('autoPosition', 'right')
+        top = targetRect.top + targetRect.height / 2 - popoverRect.height / 2
+        left = targetRect.right + this.get('offset')
+        if (excludePadding) {
+          let cs = window.getComputedStyle(targetElement)
+          left -= parseInt(cs.getPropertyValue('padding-right'))
+        }
+        popoverElement.style.top = top + 'px'
+        popoverElement.style.left = left + 'px'
       }
     }
   }
