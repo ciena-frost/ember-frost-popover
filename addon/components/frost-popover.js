87ca51a7413f9b8dc1e6dc283457cb62c54f675b
Seena <seenarowhani95@gmail.com>
2017-02-25 14:49:27 -0500

    Base changes to allow autopositioning

--- addon/components/frost-popover.js
+++ addon/components/frost-popover.js
@@ -1,74 +1,153 @@
 import Ember from 'ember'
-const {$, run} = Ember
+const {$, run, get, getProperties: getProps, setProperties: setProps} = Ember
 import {PropTypes} from 'ember-prop-types'
 import {Component} from 'ember-frost-core'
-
+import Utils, {
+  checkLeft,
+  checkRight,
+  checkTop,
+  checkBottom
+} from './util'
 import layout from '../templates/components/frost-popover'
 
 export default Component.extend({
+  // ==== Dependencies =======================================
   layout,
-
+  // ==== Properties =========================================
   propTypes: {
     event: PropTypes.string,
     classes: PropTypes.string,
     position: PropTypes.string,
     remove: PropTypes.bool,
     constrainToWindow: PropTypes.bool,
-    constrainToScrollParent: PropTypes.bool
+    constrainToScrollParent: PropTypes.bool,
+    withinBounds: PropTypes.bool
   },
 
   getDefaultProps () {
     return {
       event: 'click',
       classes: 'drop-theme-arrows-bounce',
-      position: 'bottom center',
+      position: 'bottom',
       remove: true,
       constrainToWindow: true,
-      constrainToScrollParent: true
+      constrainToScrollParent: true,
+      autoposition: true
     }
   },
+  // ==== Lifecycle Hooks =========================================
   didInsertElement () {
     this._super(...arguments)
+    const position = Utils.parsePosition(this.get('position'))
+    const auto = this.get('autoposition')
+    const constraints = []
 
+    // if (auto) {
+    //   constraints.push({
+    //     to: 'scrollParent',
+    //     attachment: 'together',
+    //     pin: true
+    //   })
+    // }
     const dropOptions = {
       target: document.querySelector(this.get('target')),
       content: this.get('element'),
       classes: this.get('classes'),
-      position: this.get('position'),
       remove: this.get('remove'),
       constrainToWindow: this.get('constrainToWindow'),
       constrainToScrollParent: this.get('constrainToWindow'),
       openOn: this.get('event'),
       tetherOptions: {
-        constraints: [
-          {
-            to: 'scrollParent',
-            attachment: 'together',
-            pin: true
-          }
-        ]
+        attachment: position.attachment,
+        targetAttachment: position.targetAttachment,
+        constraints
       }
     }
 
     const dropInstance = new Drop(dropOptions)
     dropInstance.on('open', () => {
+      run.begin()
       const onOpen = this.get('onOpen')
+
+      if (auto) {
+        this.calibrate()
+      }
+
       if (onOpen) {
         onOpen(this)
       }
+      run.end()
     })
     dropInstance.on('close', () => {
+      run.begin()
       const onClose = this.get('onClose')
       if (onClose) {
         onClose(this)
       }
+      run.end()
     })
+
+    $(dropOptions.target)
+      .blur(() => {dropInstance.close()})
+
+    $(dropOptions.content).click(function () {
+      $(dropOptions.target)
+        .click()
+        .focus()
+      return true
+    })
+    if (auto) {
+      $(window).resize(() => {
+        this.calibrate()
+      })
+    }
+
     this.set('dropInstance', dropInstance)
   },
-
   willDestroy () {
     const dropInstance = this.get('dropInstance')
     dropInstance.destroy()
     this._super(...arguments)
-  }
+  },
+  // ==== Functions =======================================
+  calibrate () {
+    const el = this.get('dropInstance')
+    const computedStyle = getComputedStyle(get(el, 'drop'))
+
+    const [xPos, yPos] = computedStyle.getPropertyValue('transform')
+      .replace(/\((.*?)\)/, '$1')
+      .split(',')
+      .map(Number)
+      .slice(-2)
+
+    const originalSettings = getProps(
+      el.tether,
+      'attachment',
+      'targetAttachment'
+    )
+    const orientation = Utils.findRoom(xPos, yPos, el.drop) || this.get('position')
+
+    const {
+      attachment,
+      targetAttachment
+    } = Utils.parsePosition(orientation)
+
+    const [top, left] = attachment.split(' ')
+    const [targetTop, targetleft] = targetAttachment.split(' ')
+
+    el.tether.attachment = {
+      top,
+      left
+    }
+    el.tether.targetAttachment = {
+      top: targetTop,
+      left: targetleft
+    }
+
+    el.tether.position()
+
+    setProps(el.tether, originalSettings)
+  },
+
+  // ==== Actions =========================================
 })
--- addon/components/util.js
+++ addon/components/util.js
@@ -1,3 +1,5 @@
+import Ember from 'ember'
+const {assert} = Ember
 /**
  * Checks if the popover can be positioned on the left
  * @param {Rect} elementPosition - the element client rect
@@ -56,4 +58,82 @@ export function checkBottom (elementPosition, popoverRect, offset, result) {
     result = 'bottom'
   }
   return result
+}
+export default {
+  positionMap: {
+    'left': {
+      attachment: 'middle right',
+      targetAttachment: 'middle left',
+      inverse: 'top'
+    },
+    'left top': {
+      attachment: 'middle right',
+      targetAttachment: 'top left',
+      inverse: 'right bottom'
+    },
+    'left bottom': {
+      attachment: 'middle right',
+      targetAttachment: 'bottom left',
+      inverse: 'right top'
+    },
+    'right': {
+      attachment: 'middle left',
+      targetAttachment: 'middle right',
+      inverse: 'left'
+    },
+    'right top': {
+      attachment: 'middle left',
+      targetAttachment: 'top right',
+      inverse: 'left bottom'
+    },
+    'right bottom': {
+      attachment: 'middle left',
+      targetAttachment: 'bottom right',
+      inverse: 'left top'
+    },
+    'top': {
+      attachment: 'bottom center',
+      targetAttachment: 'top center',
+      inverse: 'bottom'
+    },
+    'top right': {
+      attachment: 'bottom left',
+      targetAttachment: 'top left',
+      inverse: 'bottom left'
+    },
+    'top left': {
+      attachment: 'bottom right',
+      targetAttachment: 'top right',
+      inverse: 'bottom right'
+    },
+    'bottom': {
+      attachment: 'top center',
+      targetAttachment: 'bottom center',
+      inverse: 'top'
+    },
+    'bottom right': {
+      attachment: 'top left',
+      targetAttachment: 'bottom left',
+      inverse: 'top left'
+    },
+    'bottom left': {
+      attachment: 'top right',
+      targetAttachment: 'bottom right',
+      inverse: 'top right'
+    }
+  },
+  parsePosition (position) {
+    const url = "https://github.com/ciena-frost/ember-frost-popover/blob/master/README.md#placement"
+    assert(`
+      ${position} is an invalid position.
+      See ${url} for available options...
+    `, position = this.positionMap[position])
+    return position
+  },
+  findRoom (x, y, el) {
+    if (x < 0)
+      return 'right'
+    if (x + $(el).width() > window.innerWidth)
+      return 'left'
+  }
 }
