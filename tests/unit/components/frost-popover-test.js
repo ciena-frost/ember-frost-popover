29e733a94fd041458adeae7deff4a798e9b65ca2
Sophy Pal <sophy.pal@gmail.com>
2017-04-06 16:25:04 -0700

    Handle destroyed states properly

--- tests/unit/components/frost-popover-test.js
+++ tests/unit/components/frost-popover-test.js
@@ -1,6 +1,10 @@
 /* jshint expr:true */
 import {expect} from 'chai'
-import {describe, it} from 'mocha'
+import Ember from 'ember'
+const {$, run} = Ember
+import wait from 'ember-test-helpers/wait'
+import {afterEach, beforeEach, describe, it} from 'mocha'
+import sinon from 'sinon'
 
 import {unit} from 'dummy/tests/helpers/ember-test-utils/setup-component-test'
 
@@ -8,13 +12,63 @@ const test = unit('frost-popover')
 describe(test.label, function () {
   test.setup()
 
+  let component, sandbox
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create()
+  })
+
+  afterEach(function () {
+    sandbox.restore()
+  })
+
   it('calls passed action while close', function () {
     let callCounter = 0
     let forwardedAction = function () {
       callCounter++
     }
-    const component = this.subject()
+    component = this.subject()
     component.actions.close.apply(component, [forwardedAction])
     expect(callCounter).to.be.equal(1)
+  })
+
+  describe('event handlers', function () {
+    beforeEach(function () {
+      sandbox.spy($.fn, 'on')
+      sandbox.spy($.fn, 'off')
+      component = this.subject({
+        event: 'click'
+      })
+      sandbox.spy(component, 'togglePopover')
+      sandbox.spy(component, 'closePopover')
+      component.didInsertElement()
+    })
+
+    it('registers event', function () {
+      expect($.fn.on).to.have.been.calledWith('click', component._eventHandler)
+    })
+
+    it('unregisters event', function () {
+      component.willDestroyElement()
+      expect($.fn.off).to.have.been.calledWith('click', component._eventHandler)
+    })
+
+    it('does not call togglePopover when component is destroyed', function () {
+      run(() => {
+        const eventHandler = component._eventHandler
+        component.destroy()
+        eventHandler()
+        expect(component.togglePopover).to.have.callCount(0)
+      })
+    })
+
+    it('does not call closePopover when component is destroyed', function () {
+      run(() => {
+        component.destroy()
+        component.registerClickOff()
+        const eventHandler = $.fn.on.lastCall.args[1]
+        eventHandler()
+        expect(component.closePopover).to.have.callCount(0)
+      })
+    })
   })
 })
