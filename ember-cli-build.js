2b2ec60f0aed2c05dd69c5cb7bef72ce53abd41a
Sophy Pal <sophy.pal@gmail.com>
2017-04-06 16:27:09 -0700

    Fix lint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,6 +18,5 @@ module.exports = function (defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-
   return app.toTree()
 }
--- tests/unit/components/frost-popover-test.js
+++ tests/unit/components/frost-popover-test.js
@@ -2,7 +2,6 @@
 import {expect} from 'chai'
 import Ember from 'ember'
 const {$, run} = Ember
-import wait from 'ember-test-helpers/wait'
 import {afterEach, beforeEach, describe, it} from 'mocha'
 import sinon from 'sinon'
 
