9e6059593cddcf49ee63a04cdcead457f606b14c
Michael Carroll <juwara0@users.noreply.github.com>
2018-01-08 11:37:52 -0600

    remove unused ember-computed-decorators package and babel config settings

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon')
 
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
-    babel: {
-      optional: ['es7.decorators']
-    }
   })
 
   /*
